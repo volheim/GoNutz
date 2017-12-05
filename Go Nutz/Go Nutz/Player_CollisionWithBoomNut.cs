07c607353a362a94b11216b419618d2e353db096
Mikkel Richter <mikkel.rr@hotmail.com>
2017-12-05 21:05:36 +0100

    well shit

---
+++ Go Nutz/Go Nutz/Player_CollisionWithBoomNut.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Go_Nutz
+{
+    partial class Player
+    {
+        public void HandleBoomNut(BoomNut boomNut)
+        {
+            if(!boomNut.pha)
+            if (boomNut.InMotion == true)
+            {
+                boomNut.MovementVector = new Vector2(0, 0);
+                boomNut.inMotion = false;
+            }
+
+            Kick(boomNut);
+        }
+    }
+}
