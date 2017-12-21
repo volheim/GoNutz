f896edb34aea244f145a5a3485b8b37673c998ef
Lars Vedelslund <lars-spil@hotmail.com>
2017-12-21 14:49:13 +0100

    Did stuff.. doesn't work with other stuff

--- Go Nutz/Go Nutz/GameWorld(Objects).cs
+++ Go Nutz/Go Nutz/GameWorld(Objects).cs
@@ -84,7 +84,7 @@ namespace Go_Nutz
         {
             if (playerNumber == 0)
             {
-                Player1 = new Player(new Vector2(x, y), @"Images\ChipmunkWalk\ChipmunkWalk01.png;Images\ChipmunkWalk\ChipmunkWalk02.png;Images\ChipmunkWalk\ChipmunkWalk03.png;Images\ChipmunkWalk\ChipmunkWalk04.png", 10, 10, 2, 0.55f, new Keys[6] { Keys.A, Keys.S, Keys.D, Keys.W, Keys.Q, Keys.E });
+                Player1 = new Player(new Vector2(x, y), @"Images\ChipmunkWalk\ChipmunkWalk01.png;Images\ChipmunkWalk\ChipmunkWalk02.png;Images\ChipmunkWalk\ChipmunkWalk03.png;Images\ChipmunkWalk\ChipmunkWalk04.png", 1, 10, 2, 0.55f, new Keys[6] { Keys.A, Keys.S, Keys.D, Keys.W, Keys.Q, Keys.E });
                 Hometree1.HometreePlayer = Player1;
                 objects.Add(Player1);
                 playerList.Add(Player1);
@@ -92,7 +92,7 @@ namespace Go_Nutz
             }
             else if (playerNumber == 1)
             {
-                Player2 = new Player(new Vector2(x, y), @"Images\Squirrelanimation\SquiwwelWalk01.png;Images\Squirrelanimation\SquiwwelWalk02.png;Images\Squirrelanimation\SquiwwelWalk03.png;Images\Squirrelanimation\SquiwwelWalk04.png;Images\Squirrelanimation\SquiwwelWalk05.png;Images\Squirrelanimation\SquiwwelWalk06.png;Images\Squirrelanimation\SquiwwelWalk07.png;Images\Squirrelanimation\SquiwwelWalk08.png", 10, 10, 10, 0.55f, new Keys[6] { Keys.J, Keys.K, Keys.L, Keys.I, Keys.U, Keys.O });
+                Player2 = new Player(new Vector2(x, y), @"Images\Squirrelanimation\SquiwwelWalk01.png;Images\Squirrelanimation\SquiwwelWalk02.png;Images\Squirrelanimation\SquiwwelWalk03.png;Images\Squirrelanimation\SquiwwelWalk04.png;Images\Squirrelanimation\SquiwwelWalk05.png;Images\Squirrelanimation\SquiwwelWalk06.png;Images\Squirrelanimation\SquiwwelWalk07.png;Images\Squirrelanimation\SquiwwelWalk08.png", 1, 10, 10, 0.55f, new Keys[6] { Keys.J, Keys.K, Keys.L, Keys.I, Keys.U, Keys.O });
                 Hometree2.HometreePlayer = Player2;
                 objects.Add(Player2);
                 playerList.Add(Player2);
